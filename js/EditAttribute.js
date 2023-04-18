69c5a54dad3d1712286d27ecc4034910a1548a5e
Trever B <tjbruhn@wisc.edu>
2023-04-17 17:27:39 -0700

    refactor submission validation in edit attributes

--- js/EditAttribute.js
+++ js/EditAttribute.js
@@ -1,10 +1,10 @@
 import { getAttributes } from "./GetAttributes.js";
 
 export function editAttribute(layer, graphic, attrName) {
-  //close any open popup window
+  // close any open popup window
   $(".edit").css("display", "none");
 
-  //disable other edit buttons while pop up is open
+  // disable other edit buttons while pop up is open
   $(".editBtn").prop("disabled", true);
 
   // reactivate submit button disabled in form validation
@@ -13,13 +13,32 @@ export function editAttribute(layer, graphic, attrName) {
   let attrValue = graphic.attributes[attrName];
   let valueID = "#textEdit";
 
-  //change the text in the label field to reflect the item being edited
+  // change the text in the label field to reflect the item being edited
   let editBoxLabel = "Edit: " + attrName;
   $(".edit label").html(editBoxLabel);
 
-  //insert form entry variation based on attrName
+  // insert form entry variation based on attrName
+
   //lists for form variations
   const yesNo = ["Sprinklers", "Detectors", "Electricity", "Water", "Gas"];
+
+  function validateSubmit() {
+    //inactivate submit button until Valid entry is supplied
+    //  if no current value disable submit button
+    if (!attrValue) {
+      $("#editSubmit").prop("disabled", true);
+    }
+    //  when a valid value is entered enable submit button
+    $("#textEdit")
+      .off()
+      .on("keyup", function () {
+        if ($("#textEdit")[0].checkValidity()) {
+          $("#editSubmit").prop("disabled", false);
+        } else {
+          $("#editSubmit").prop("disabled", true);
+        }
+      });
+  }
 
   //establish lists of attrNames that recieve a specific form type
   if (attrName === "POCNumber") {
@@ -29,48 +48,21 @@ export function editAttribute(layer, graphic, attrName) {
        required><span class="validity"></span><br/><small>Format: 123-456-7890</small>`
     );
 
-    //inactivate submit button until Valid entry is supplied
-    //  if no current value disable submit button
-    if (!attrValue) {
-      $("#editSubmit").prop("disabled", true);
-    }
-
-    //  when a valid value is entered enable submit button
-    $("#textEdit")
-      .off()
-      .on("keyup", function () {
-        if ($("#textEdit")[0].checkValidity()) {
-          $("#editSubmit").prop("disabled", false);
-        } else {
-          $("#editSubmit").prop("disabled", true);
-        }
-      });
+    validateSubmit();
   } else if (yesNo.includes(attrName)) {
     $("#formVariant").html(
       `<input type="radio" name="yes_no" value="Yes" >Yes</input><input type="radio" name="yes_no" value="No">No</input>`
     );
     valueID = 'input[name="yes_no"]:checked';
+
     //set the checked radio to the current value
     $('input[name="yes_no"]').val([attrValue]);
   } else if (attrName === "SqFt") {
     $("#formVariant").html(
       `<input type="text" inputmode="numeric" pattern="[0-9]*" minlength="3" id="textEdit" required></input><span class="validity"></span>`
     );
-    //inactivate submit button until Valid entry is supplied
-    //  if no current value disable submit button
-    if (!attrValue) {
-      $("#editSubmit").prop("disabled", true);
-    }
-    //  when a valid value is entered enable submit button
-    $("#textEdit")
-      .off()
-      .on("keyup", function () {
-        if ($("#textEdit")[0].checkValidity()) {
-          $("#editSubmit").prop("disabled", false);
-        } else {
-          $("#editSubmit").prop("disabled", true);
-        }
-      });
+
+    validateSubmit();
   } else if (attrName === "Campus") {
     $("#formVariant").html(
       `<select id="textEdit">
@@ -106,28 +98,33 @@ export function editAttribute(layer, graphic, attrName) {
       `<textarea id="textEdit" name="textEdit" rows="10" cols=""></textarea>`
     );
   }
+
   //populate text edit box with the current value
   $("#textEdit").val(attrValue);
 
-  //disply the pop up edit window
+  //display the pop up edit window
   $(".edit").css("display", "block");
 
+  // Set Submit button function
   $("#editSubmit")
     .off()
     .on("click", function () {
       let newAttrValue = $(valueID).val();
 
-      console.log(
-        "submitting id:" +
-          attrName +
-          " with new value: " +
-          newAttrValue +
-          " valueID: " +
-          valueID
-      );
+      // console.log(
+      //   "submitting id:" +
+      //     attrName +
+      //     " with new value: " +
+      //     newAttrValue +
+      //     " valueID: " +
+      //     valueID
+      // );
+
       //update graphic with new value
       graphic.attributes[attrName] = newAttrValue;
-      console.log("with edits: ", graphic.attributes);
+      // console.log("with edits: ", graphic.attributes);
+
+      // post the edits
       layer
         .applyEdits({
           updateFeatures: [graphic],
@@ -137,17 +134,20 @@ export function editAttribute(layer, graphic, attrName) {
         })
         .then(() => {
           //update form with value supplied
-          //$("#" + item).html($("#textEdit").val());
           getAttributes(layer, graphic);
         })
         .catch((error) => {
           console.log("error = ", error);
         });
+
       //close popup window
       $(".edit").css("display", "none");
+
       //enable other edit buttons on submit
       $(".editBtn").prop("disabled", false);
     });
+
+  // Set Cancel function
   $("#editCancel").on("click", function () {
     //close popup window
     $(".edit").css("display", "none");
